c2d2b3ba5a955fe4c2090a5410904341b620b7af
Ian Bravyn <ianbravynsa@gmail.com>
2023-05-31 12:15:01 +0300

    added js code to toggle navbar css class on scroll to achieve a transition in color

--- main.js
+++ main.js
@@ -1,33 +1,11 @@
-// Function to check if an element is in the viewport
-function isElementInViewport(element) {
-    var rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-  
-  // Function to handle scroll event
-  const handleScroll = () => {
-    var elements = document.getElementsByClassName('categories');
-    
-    for (var i = 0; i < elements.length; i++) {
-      var element = elements[i];
-      
-      if (isElementInViewport(element)) {
-        element.classList.add('categories');
-      }
-    }
-  }
+
+const nav = document.getElementsByTagName('nav')[0];
   
   // Add scroll event listener
-window.addEventListener('scroll',  handleScroll);
+
 addEventListener('scroll', () => {
-    const nav = document.getElementsByTagName('nav')[0];
-    nav.style.backgroundColor = "var(--color-bg1)";
+    nav.classList.toggle('on__scroll', scrollY > 0)
 }) 
   // Trigger the animation for elements already in view
-handleScroll()
+
   
